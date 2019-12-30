c449ed88ab9659d1c784604061edd79cbfa56172
LeeKangWoo ( LapTop ) <leegw1371@gmail.com>
2019-12-31 00:10:07 +0900

    로딩 씬 구현 완료
    1. 로딩 프로그래스 바 적용 완료
    2. 씬 전환 시 로딩 씬 적용 완료

--- KPU_Project/Assets/Scenes/Others/Script/MainScene_Manager.cs
+++ KPU_Project/Assets/Scenes/Others/Script/MainScene_Manager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Manager
 {
@@ -9,19 +12,57 @@ namespace Manager
     /// </summary>
     public class MainScene_Manager : MonoBehaviour
     {
-        private static MainScene_Manager _singleton;
-        public static MainScene_Manager mainscene_manager
-        {
-            get { return _singleton; }
-        }
-
-        public bool Is_loading
-        {
-            get;set;
-        }
+        #region Canvas
+        [Header("Canvas")]
+        [Tooltip("loading canvas")]
         [SerializeField] private GameObject Loading_canvas;
+
+        [Tooltip("ID PW canvas")]
         [SerializeField] private GameObject ID_PW_canvas;
+
+        [Tooltip("Menu canvas")]
         [SerializeField] private GameObject Menu_canvas;
+
+        #endregion
+
+        #region Scene
+        [Header("Scene Name")]
+
+        [Tooltip("VR 씬 이름")]
+        [SerializeField] private string VR_Scene_Name;
+
+        [Tooltip("TOBII 씬 이름")]
+        [SerializeField] private string TOBII_Scene_Name;
+
+        [Tooltip("Recrod 씬 이름")]
+        [SerializeField] private string Recrod_Scene_Name;
+        #endregion
+
+        #region 로딩 프로그레스 ( 숫자 있는 거 )
+
+        [Header("Loading Num Progress")]
+        [SerializeField] private Loading_Num_progress num_progress;
+
+        [System.Serializable]
+        public struct Loading_Num_progress{
+        [Header("Loading Object")]
+        [Tooltip("로딩 Progress")]
+        public GameObject loading_object;
+        [Tooltip("로딩 Progress 숫자 있음")]
+         public GameObject loading_withnum_object;
+
+        [Header("Loading Image")]
+        public RectTransform rectComponent;
+        public Image imageComp;
+
+        [Header("Loading Text")]
+        public Text text_num;
+        public Text text_string;
+
+        }
+        #endregion
+
+        #region 프로퍼티
         /// <summary>
         /// 로그인 되었는지 아닌지를 체크해준다.
         /// </summary>
@@ -29,10 +70,26 @@ namespace Manager
         {
             get;set;
         }
+        public bool Is_loading
+        {
+            get; set;
+        }
+        #endregion
+
+        #region 싱글 톤
+
+        private static MainScene_Manager _singleton;
+        public static MainScene_Manager mainscene_manager
+        {
+            get { return _singleton; }
+        }
+
         private void Awake()
         {
             _singleton = GetComponent<MainScene_Manager>();
         }
+        #endregion
+
         private void Start()
         {
             Is_loading = false;
@@ -42,7 +99,15 @@ namespace Manager
             Menu_canvas.GetComponent<Canvas>().planeDistance = 60f;
             Loading_canvas.GetComponent<Canvas>().planeDistance = 70f;
 
-            
+            Menu_canvas.SetActive(false);
+
+
+            num_progress.loading_object.SetActive(true);
+            num_progress.loading_withnum_object.SetActive(false);
+
+            num_progress.text_num.text = 0.ToString() + "%";
+
+            num_progress.text_string.text = "DownLoading";
         }
         
        /// <summary>
@@ -54,6 +119,12 @@ namespace Manager
         public void check_userinformation<T>(T ID, T PW) where T : class
         {
             Show_Loading_Canvas();
+            StartCoroutine(Check_Database());
+        }
+        IEnumerator Check_Database()
+        {
+            yield return new WaitForSeconds(1f);
+            Show_Menu_Canvas();
         }
 
         /// <summary>
@@ -70,19 +141,94 @@ namespace Manager
         /// </summary>
         private void Hide_Loading_Canvas()
         {
-            Is_login = false;
+            Is_loading = false;
             Loading_canvas.GetComponent<Canvas>().planeDistance = 70f;
         }
 
         private void Show_Menu_Canvas()
         {
-            
             ID_PW_canvas.GetComponent<Canvas>().planeDistance = 100f;
             ID_PW_canvas.SetActive(false);
 
             Hide_Loading_Canvas();
+            num_progress.loading_object.SetActive(false); // 그저 도는건 꺼주기.
+            num_progress.loading_withnum_object.SetActive(true); // 숫자 로딩 켜준다. ( 메뉴부터는 씬이 바뀌는 거기 때문 )
 
+            Menu_canvas.SetActive(true);
             Menu_canvas.GetComponent<Canvas>().planeDistance = 50f;
+        }
+
+        /// <summary>
+        /// 씬 전환 함수
+        /// </summary>
+        /// <param name="btn_name"> 버튼 누른것에 맞춰서 맞는 씬으로 이동 </param>
+        public void GO_Scene(string btn_name)
+        {
+            AsyncOperation async;
+            if (btn_name.Contains("VR"))
+            {
+                async = SceneManager.LoadSceneAsync(VR_Scene_Name);
+            }
+            else if (btn_name.Contains("TOBII"))
+            {
+                async = SceneManager.LoadSceneAsync(TOBII_Scene_Name);
+            }
+            else
+            {
+                async = SceneManager.LoadSceneAsync(Recrod_Scene_Name);
+            }
+
+            StartCoroutine(loading_SceneChanged(async));
+            
+        }
+
+        IEnumerator loading_SceneChanged(AsyncOperation async)
+        {
+            Show_Loading_Canvas();
+          float timer = 0;
+
+          num_progress.imageComp = num_progress.rectComponent.GetComponent<Image>(); // 이미지 세팅
+          num_progress.imageComp.fillAmount = 0.0f; // 초기화 시켜준다.
+
+          async.allowSceneActivation = false;
+
+            while (!async.isDone)
+            {
+                yield return null;
+
+                timer += Time.deltaTime;
+
+                if (async.progress >= 0.9f)
+                {
+                    num_progress.text_string.text = "Please wait...";
+                    num_progress.imageComp.fillAmount = Mathf.Lerp(num_progress.imageComp.fillAmount, 1f, timer);
+                    num_progress.text_num.text = (Math.Truncate(num_progress.imageComp.fillAmount * 100)).ToString() + "%";
+
+                    if (num_progress.imageComp.fillAmount == 1f)
+                    {
+                        async.allowSceneActivation = true;
+                    }
+                }
+                else
+                {
+                    if(num_progress.imageComp.fillAmount < 0.5f)
+                    {
+                        num_progress.text_string.text = "DownLoading";
+                    }
+                    else if(num_progress.imageComp.fillAmount< 0.9f)
+                    {
+                        num_progress.text_string.text = "Loading";
+                    }
+
+                    num_progress.imageComp.fillAmount = Mathf.Lerp(num_progress.imageComp.fillAmount, async.progress, timer);
+                    num_progress.text_num.text = (Math.Truncate(num_progress.imageComp.fillAmount * 100)).ToString() + "%";
+
+                    if (num_progress.imageComp.fillAmount>= async.progress)
+                    {
+                        timer = 0f;
+                    }
+                }
+            }
         }
         
     }
---
+++ KPU_Project/Assets/Scenes/Others/Script/Menu_Canvas_Script/Click_Menu_Button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+
+namespace Button
+{
+    /// <summary>
+    /// 
+    /// 메뉴 버튼 클릭 시, 씬 전환 해주는 함수
+    /// 
+    /// 씬 이름을 Inspector에 작성해야 함.
+    /// 
+    /// </summary>
+    public class Click_Menu_Button : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Manager.MainScene_Manager.mainscene_manager.GO_Scene(gameObject.name);
+        }
+    }
+}
